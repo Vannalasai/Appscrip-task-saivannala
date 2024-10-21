46e82d2ca680e5be35a4e8a3c5d1b07171484a56
vannala sai <saikrishna.vannala@gmail.com>
2024-10-21 23:02:15 +0530

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("web pages")
+
+async function Function(){
+  return 1;
+}
+console.log(Function())
+
+const list = [1, 2, 3, 4, 5];
+
+const some = list.some(num => num % 2 === 0)
+
+console.log(some)
+
+const every = list.every(num => num % 2 === 0)
+
+console.log(every)
+
+const addFirstElement = list.unshift(0)
+console.log(addFirstElement)
+console.log(list)
+
+const removeFirstElement = list.shift()
+console.log(removeFirstElement)
+console.log(list)
