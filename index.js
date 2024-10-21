41e3c2815f1d1a28264d351ff2dfd7aef25dd58b
vannala sai <saikrishna.vannala@gmail.com>
2024-10-21 23:32:46 +0530

    fouth commit

--- index.js
+++ index.js
@@ -22,3 +22,23 @@ console.log(list)
 const removeFirstElement = list.shift()
 console.log(removeFirstElement)
 console.log(list)
+
+const addLastElement = list.push(6)
+console.log(addLastElement)
+console.log(list)
+
+const removeLastElement = list.pop(6)
+console.log(removeLastElement)
+console.log(list)
+
+setTimeout(() => {
+  console.log(list)
+},2000)
+
+const getData = (list) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(list), 1000)
+  })
+}
+
+getData(list).then(result => console.log(result)).catch(error => console.log(error))
