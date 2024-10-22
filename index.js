c1d8432e4a5a14fe01ad3bb97794fb95c3917f94
vannala sai <saikrishna.vannala@gmail.com>
2024-10-22 23:27:34 +0530

    five commit

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ setTimeout(() => {
   console.log(list)
 },2000)
 
+//promise in asynchronous function
+
 const getData = (list) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve(list), 1000)
@@ -42,3 +44,62 @@ const getData = (list) => {
 }
 
 getData(list).then(result => console.log(result)).catch(error => console.log(error))
+
+//Curring Function
+
+function multiply(a) {
+  return function (b) {
+      return a * b;
+  }
+}
+
+const doubled = multiply(2);
+console.log(doubled(3))
+
+function mutiple(a) {
+  return function (b) {
+    return function (c) {
+      return a * b * c;
+    }
+  }
+}
+
+const triple = mutiple(2)
+const single = triple(3)
+console.log(single(4))
+
+// function sayHi() {
+
+//   console.log(name)
+//   console.log(age)
+//   var name = "sai";
+//   let age = 24;
+// }
+
+// sayHi();
+
+const obj = [
+  {name: "Sai", age: 24},
+  {name: "Deepu", age: 16},
+  {name: "jonny", age: 21},
+  {name: "lilly", age: 28},
+  {name: "prabhas", age: 45},
+  {name: "Sai", age: 24},
+  {name: "prabhas", age: 45},
+  {name: "Deepu", age: 16},
+  {name: "Navvu", age: 21}
+]
+
+const nameCount = {};
+
+obj.forEach((item) =>{
+  if(item.age >= 18){
+    if(nameCount[item.age]){
+      nameCount[item.age]++;
+    } else {
+      nameCount[item.age] = 1;
+    }
+  }
+})
+
+console.log(nameCount)
